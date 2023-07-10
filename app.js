24fe5648a4359e60e93eb6b0cd4aa432a6e57baa
Geometr <geo.fsv.p@gmail.com>
2023-07-10 23:47:07 +0300

    feat: подклюена mongoDB

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const mogoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+mogoose.connect('mongodb://127.0.0.1:27017/mestodb').catch(console.log);
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
