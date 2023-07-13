592119c5975788214e6b0efbbb2427ae4a08cbe7
GeometricEath <geo.fsv@gmail.com>
2023-07-13 17:51:49 +0300

    feat: обновление профиля пользователя

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,29 @@
 const { default: mongoose } = require('mongoose');
 const User = require('../models/user');
 
+const updateUser = (req, res, body) => {
+  User.findByIdAndUpdate(
+    req.user._id,
+    { $set: { ...body } },
+    // eslint-disable-next-line comma-dangle
+    { new: true, runValidators: true }
+  )
+    .then((result) => {
+      res.status(200).send(result);
+    })
+    .catch((err) => {
+      if (
+        // eslint-disable-next-line operator-linebreak
+        err instanceof mongoose.Error.ValidationError ||
+        err instanceof mongoose.Error.CastError
+      ) {
+        res.status(400).send({ message: 'Переданы некоректные данные', err });
+        return;
+      }
+      res.status(500).send({ message: 'Внутреняя ошибка сервера', err });
+    });
+};
+
 const getAllUsers = (req, res) => {
   User.find({})
     .then((users) => {
@@ -11,7 +34,7 @@ const getAllUsers = (req, res) => {
         res.status(400).send({ message: 'Ошибка в данных запроса' });
         return;
       }
-      res.status(500).sent({ message: 'Внутреняя ошибка сервера', err });
+      res.status(500).send({ message: 'Внутреняя ошибка сервера', err });
     });
 };
 
@@ -31,27 +54,25 @@ const getUserById = (req, res) => {
         res.status(400).send({ message: 'Ошибка в данных запроса' });
         return;
       }
-      res.status(500).sent({ message: 'Внутреняя ошибка сервера', err });
+      res.status(500).send({ message: 'Внутреняя ошибка сервера', err });
     });
 };
 
 const updateUserProfile = (req, res) => {
-  User.findByIdAndUpdate(
-    req.user._id,
-    { $set: { ...req.body } },
-    // eslint-disable-next-line comma-dangle
-    { new: true }
-  )
-    .then((result) => {
-      res.status(200).send(result);
-    })
-    .catch((err) => {
-      if (err instanceof mongoose.Error.ValidationError) {
-        res.status(400).send({ message: 'Переданы некоректные данные', err });
-        return;
-      }
-      res.status(500).setn({ message: 'Внутреняя ошибка сервера', err });
-    });
+  if ('avatar' in req.body) {
+    res.status(400).send({ message: 'Переданы некоректные данные' });
+    return;
+  }
+
+  updateUser(req, res, req.body);
+};
+
+const updateUserAvatar = (req, res) => {
+  if (req.body.avatar || req.body.name || req.body.about) {
+    res.status(400).send({ message: 'Переданы некоректные данные' });
+  }
+
+  updateUser(req, res, { avatar: req.body.avatar });
 };
 
 const addUser = (req, res) => {
@@ -64,7 +85,7 @@ const addUser = (req, res) => {
         res.status(400).send({ message: 'Переданы некоректные данные', err });
         return;
       }
-      res.status(500).setn({ message: 'Внутреняя ошибка сервера', err });
+      res.status(500).send({ message: 'Внутреняя ошибка сервера', err });
     });
 };
 
@@ -73,4 +94,5 @@ module.exports = {
   getUserById,
   addUser,
   updateUserProfile,
+  updateUserAvatar,
 };
--- routes/users.js
+++ routes/users.js
@@ -5,11 +5,13 @@ const {
   getUserById,
   addUser,
   updateUserProfile,
+  updateUserAvatar,
 } = require('../controllers/users');
 
 router.get('/', getAllUsers);
 router.get('/:userId', getUserById);
 router.post('/', addUser);
 router.patch('/me', updateUserProfile);
+router.patch('/me/avatar', updateUserAvatar);
 
 module.exports = router;
