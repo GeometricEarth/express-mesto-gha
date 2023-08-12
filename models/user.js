f17a59e3e2cc6e2c8135d6ffc17dc85d7608844c
Geometr <geo.fsv.p@gmail.com>
2023-08-12 20:10:34 +0300

    fix: исправлена работа валидатора на уровне схемы

--- models/user.js
+++ models/user.js
@@ -18,8 +18,9 @@ const userSchema = new mongoose.Schema({
     type: String,
     default:
       'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    // eslint-disable-next-line arrow-body-style
     validate: (value) => {
-      /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]+\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/.test(
+      return /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]+\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/.test(
         value,
       );
     },
