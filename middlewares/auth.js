fa3da5a1665f18b43f9ede2d32daf11db59b9371
Geometr <geo.fsv.p@gmail.com>
2023-08-12 23:30:54 +0300

    fix: обработка ошибок перенесена в централизованный мидлвэре

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,11 +1,13 @@
 const jwt = require('jsonwebtoken');
 
+const AuthError = require('../utils/httpErrors/AuthError');
+
 const userAuth = (req, res, next) => {
   try {
     const payload = jwt.verify(req.cookies.jwt, 'dev-secret');
     req.user = payload;
   } catch (err) {
-    return res.status(401).send({ message: 'Необходима авторизация' });
+    next(new AuthError('Необходима авторизация'));
   }
 
   return next();
