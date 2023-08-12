509ba4c1bf1b97d00e4d9ffa232261465e016728
Geometr <geo.fsv.p@gmail.com>
2023-08-12 19:26:59 +0300

    feat: Валидация данных во входящих запросах

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const { celebrate, Joi, errors } = require('celebrate');
 
 const usersRoutes = require('./routes/users');
 const cardsRoutes = require('./routes/cards');
@@ -23,8 +24,31 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post(
+  '/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().min(6),
+      password: Joi.string().required().min(6),
+    }),
+  }),
+  login,
+);
+app.post(
+  '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required().min(6),
+      about: Joi.string().min(2).max(30),
+      name: Joi.string().min(2).max(30),
+      avatar: Joi.string().pattern(
+        /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]+\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/,
+      ),
+    }),
+  }),
+  createUser,
+);
 app.use('/users', userAuth, usersRoutes);
 app.use('/cards', userAuth, cardsRoutes);
 
@@ -33,6 +57,8 @@ app.use((_req, res) => {
     .status(404)
     .send({ message: 'Страница которую вы запрашиваете не существует' });
 });
+
+app.use(errors());
 
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, _next) => {
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { Joi, celebrate } = require('celebrate');
 
 const {
   getAllUsers,
@@ -9,8 +10,35 @@ const {
 
 router.get('/', getAllUsers);
 router.get('/me', getUserById);
-router.get('/:userId', getUserById);
-router.patch('/me', updateUserProfile);
-router.patch('/me/avatar', updateUserAvatar);
+router.get(
+  '/:userId',
+  celebrate({
+    params: Joi.object().keys({
+      userId: Joi.string().hex().length(24).required(),
+    }),
+  }),
+  getUserById,
+);
+router.patch(
+  '/me',
+  celebrate({
+    body: Joi.object().keys({
+      about: Joi.string().min(2).max(30),
+      name: Joi.string().min(2).max(30),
+    }),
+  }),
+  updateUserProfile,
+);
+router.patch(
+  '/me/avatar',
+  celebrate({
+    body: Joi.object().keys({
+      avatar: Joi.string().pattern(
+        /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]+\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/,
+      ).required(),
+    }),
+  }),
+  updateUserAvatar,
+);
 
 module.exports = router;
