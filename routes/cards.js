34a25d64f3b5a784162589c9d078f7a77e5bce1f
Geometr <geo.fsv.p@gmail.com>
2023-08-13 14:49:13 +0300

    feat: Ревью 3

--- routes/cards.js
+++ routes/cards.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Joi, celebrate } = require('celebrate');
+const { urlRegExp } = require('../utils/constants');
 
 const {
   createCard,
@@ -15,11 +16,7 @@ router.post(
   celebrate({
     body: Joi.object().keys({
       name: Joi.string().required().min(2).max(20),
-      link: Joi.string()
-        .pattern(
-          /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]+\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/,
-        )
-        .required(),
+      link: Joi.string().pattern(urlRegExp).required(),
     }),
   }),
   createCard,
