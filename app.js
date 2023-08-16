f221f8d6d1d8d4fb0fcc958b762b2846a53d217b
GeometricEath <geo.fsv@gmail.com>
2023-08-16 12:19:15 +0300

    fix: сменен порт по умолчанию на 3001

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const { urlRegExp } = require('./utils/constants');
 const errorHandler = require('./middlewares/errorHandler');
 const NotFoundError = require('./utils/httpErrors/NotFound');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 
 const app = express();
 mongoose
